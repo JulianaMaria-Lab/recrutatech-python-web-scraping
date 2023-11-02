341a678b9d50b72ebf7f87467f8eac6df47a10d9
MayconRafael <mayconrafacruz@gmail.com>
2023-11-02 11:37:19 -0300

    matching without AI

--- raspagem_candidatos.py
+++ raspagem_candidatos.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import requests
 from bs4 import BeautifulSoup
+import re
 
 link = "http://localhost:8000"
 headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36"}
@@ -25,29 +26,8 @@ def qtd_pessoa_profissao(a):
             cont = cont + 1
     print(cont)
 
-def mostrarSobre():
-    for x in sobre:
-        print(x.text+
-              "\n----------------------------------------------------------------------------------------------------------------\n", 
-              x.next_sibling)
 
-def mostrarSobrePessoa(a):
-    z = []
-    r = []
-    t = 0
-    for x in pessoa:
-        z.append(x)
-        if(a == x.text): 
-            t = len(z)
-            break;     
-    for y in sobre: 
-        r.append(y.text) 
-    if(t == 0):
-        print("Pessoa não existe!")
-    else:
-        print(r[t-1]) 
-
-def mostrarSobreProfissao(a):
+def mostrarCodigoProfissao(a):
     z = []
     r = []
     o = []
@@ -56,34 +36,71 @@ def mostrarSobreProfissao(a):
         z.append(x.text)
 
     for d in z:
-        if (a == d):
-            for e in sobre:
+        if (a["cargo"].lower() == d.lower()):
+            for e in codigo:
                 r.append(e.text)
-            print(r[t]+"\n----------------------------------------------------------------------------------------------------------------\n"+
-                  "----------------------------------------------------------------------------------------------------------------\n"+
-                  "----------------------------------------------------------------------------------------------------------------\n"+
-                  "----------------------------------------------------------------------------------------------------------------\n"+
-                  "----------------------------------------------------------------------------------------------------------------\n")
+            o.append(r[t])
 
         t = t + 1
-    return jsonify(r)
+    return jsonify(o)
 
+def matchProfissao(a):
+    lista_profissao = []
+    lista_codigo = []
+    lista_sobre = []
+    codigos = []
+    sobres = []
+    verificacao = []
+    matchs = []
+    contador = 0
+    v_conhecimento = 0
+    v_habilidade = 0
+    v_atitude = 0
 
-def mostrarProfissaoPessoa(a):
-    z = []
-    r = []
-    t = 0
-    for x in pessoa:
-        z.append(x)
-        if(a == x.text): 
-            t = len(z)
-            break;     
-    for y in profissao: 
-        r.append(y.text) 
-    if(t == 0):
-        print("Pessoa não existe!")
-    else:
-        print(r[t-1])
+    for p in profissao:
+        lista_profissao.append(p.text)
+
+    for p in lista_profissao:
+        if (a["cargo"].lower() == p.lower()):
+            for s in sobre:
+                so = s.text
+                sob = re.sub(r'[.,"\'-?:!;]', '', so)
+                lista_sobre.append(sob)
+            sobres.append(lista_sobre[contador])
+
+            for c in codigo:
+                lista_codigo.append(c.text)
+            codigos.append(lista_codigo[contador])
+
+        contador = contador + 1
+    
+    contador = 0
+    
+    for s in sobres:
+        for palavra in s.split():
+            for conhecimento in a["conhecimentos"]:
+                if(palavra.lower() == conhecimento.lower()):
+                    v_conhecimento = v_conhecimento + 1
+
+            for habilidade in a["habilidades"]:
+                if(palavra.lower() == habilidade.lower()):
+                    v_habilidade = v_habilidade + 1
+
+            for atitude in a["atitudes"]:
+                if(palavra.lower() == atitude.lower()):
+                    v_atitude = v_atitude + 1
+
+        v = [v_conhecimento, v_habilidade, v_atitude]
+        verificacao.append(v)
+        total = v_conhecimento + v_habilidade + v_atitude 
+        if(total > 2):
+            matchs.append(codigos[contador])
+        contador = contador + 1
+        v_conhecimento = 0
+        v_habilidade = 0
+        v_atitude = 0
+                
+    return jsonify(verificacao)
                  
 
 app = Flask(__name__)
@@ -92,8 +109,9 @@ CORS(app)
 @app.route('/scraping', methods=['POST'])
 def ask_question():
     try:
-        data = request.data.decode('utf-8')
-        return mostrarSobreProfissao(data)
+        # data = request.data.decode('utf-8')
+        data = request.json
+        return matchProfissao(data)
 
     except Exception as e:
         return jsonify({"error": str(e)})
